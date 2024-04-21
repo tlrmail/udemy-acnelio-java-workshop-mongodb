cc12acd2f9c8bf9757921517b9f3d6990661df73
thiago.leite <thiago.leite@dataprev.gov.br>
2024-04-20 23:12:44 -0300

    Obtendo um usuário por id

--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.nelioalves.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.workshopmongo.domain.User;
 import com.nelioalves.workshopmongo.repository.UserRepository;
+import com.nelioalves.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,14 @@ public class UserService {
 		return repository.findAll();
 	}
 
+	public User findById(String id) {
+		User user = null;
+		Optional<User> optional = repository.findById(id);
+		if(optional.isEmpty()) {
+			throw new ObjectNotFoundException("Objeto não encontrado.");
+		}else {
+			user = optional.get();
+		}
+		return user;
+	}
 }
